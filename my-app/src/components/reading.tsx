8532a75a74240e944dd46c0124007d510b86b11d
Nguyen Bao <bao@swapcard.com>
2023-05-06 00:11:07 +0200

    style and darkmode

--- my-app/src/components/reading.tsx
+++ my-app/src/components/reading.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 import { VscChromeClose } from "react-icons/vsc";
+import { MdModeNight } from "react-icons/md";
 import { useFetchData } from "../helper/useFetchData.ts";
 import "./reading.css";
 
@@ -10,17 +11,31 @@ const PageWrapper = styled.div`
   flex-direction: column;
   height: 100vh;
   overflow-y: auto;
+  font-size: ${({ fontSize }) => fontSize || FONTSIZE}px;
+  line-height: ${({ fontSize }) => fontSize + 5 || FONTSIZE + 5}px;
+  background: ${({ mode }) => (mode === DARK_MODE ? "#232323" : "white")};
+  color: ${({ mode }) => (mode === DARK_MODE ? "white" : "black")};
 `;
 
 const CloseButton = styled(VscChromeClose)`
   position: fixed;
-  color: black;
+  color: ${({ mode }) => (mode === DARK_MODE ? "white" : "black")};
   right: 16px;
   top: 16px;
   z-index: 1;
   cursor: pointer;
   width: 1.2rem;
   height: 1.2rem;
+`;
+
+const DarkMode = styled(MdModeNight)`
+  z-index: 1;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  padding: 4px 8px;
+  background: #e3e3e3;
+  border-radius: 8px;
 `;
 
 const LoaderContent = styled.div`
@@ -33,7 +48,6 @@ const LoaderContent = styled.div`
 const Content = styled.div`
   padding: 70px 24px;
   font-family: sans-serif;
-  line-height: 22px;
   margin-bottom: 80px;
 `;
 
@@ -46,7 +60,7 @@ const Footer = styled.div`
   gap: 8px;
   padding: 8px 24px;
   justify-content: center;
-  background: white;
+  background: ${({ mode }) => (mode === DARK_MODE ? "#232323" : "white")};
   opacity: 0.95;
 `;
 
@@ -91,14 +105,21 @@ function Loader() {
   );
 }
 
+const FONTSIZE = 18;
+const DARK_MODE = "dark";
+const LIGHT_MODE = "light";
+
 function Reading() {
-  console.log(localStorage);
   const [chapter, setChapter] = useState(
     parseInt(localStorage.getItem("chapter")) || 1
   );
   const [scrollPosition, setScrollPosition] = useState(
     localStorage.getItem("scrollPosition") || 0
   );
+  const [fontSize, setFontSize] = useState(
+    parseInt(localStorage.getItem("fontSize")) || FONTSIZE
+  );
+  const [mode, setMode] = useState(localStorage.getItem("mode") || LIGHT_MODE);
 
   useEffect(() => {
     function handleBeforeUnload() {
@@ -115,6 +136,8 @@ function Reading() {
   const saveLocalStorage = () => {
     localStorage.setItem("chapter", chapter);
     localStorage.setItem("scrollPosition", scrollPosition);
+    localStorage.setItem("fontSize", fontSize);
+    localStorage.setItem("mode", mode);
   };
 
   const handleScroll = (event) => {
@@ -122,15 +145,26 @@ function Reading() {
   };
 
   return (
-    <PageWrapper onScroll={handleScroll}>
+    <PageWrapper onScroll={handleScroll} fontSize={fontSize} mode={mode}>
       <Link to="/">
-        <CloseButton onClick={saveLocalStorage} />
+        <CloseButton mode={mode} onClick={saveLocalStorage} />
       </Link>
       <View chapter={chapter} />
-      <Footer>
+      <Footer mode={mode}>
         <Button onClick={() => setChapter((old) => (old > 1 ? old - 1 : old))}>
           Previous
         </Button>
+        <Button onClick={() => setFontSize((font) => font + 1)}>A+</Button>
+        <Button
+          onClick={() => setFontSize((font) => (font > 16 ? font - 1 : font))}
+        >
+          A-
+        </Button>
+        <DarkMode
+          onClick={() =>
+            setMode((mode) => (mode === LIGHT_MODE ? DARK_MODE : LIGHT_MODE))
+          }
+        />
         <Button onClick={() => setChapter((old) => old + 1)}>Next</Button>
       </Footer>
     </PageWrapper>
